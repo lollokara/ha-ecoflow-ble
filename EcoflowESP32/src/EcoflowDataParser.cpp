4e57aa96f37037fc4f47b4b02c470f62ed1069df
lollokara <lollo.kl@hotmail.it>
2025-11-20 19:30:40 +0100

    Few fixes and reduced logging

--- EcoflowESP32/src/EcoflowDataParser.cpp
+++ EcoflowESP32/src/EcoflowDataParser.cpp
@@ -21,18 +21,18 @@ namespace EcoflowDataParser {
 
 void parsePacket(const Packet& pkt, EcoflowData& data) {
     if (pkt.getSrc() == 0x02 && pkt.getCmdSet() == 0xFE && (pkt.getCmdId() == 0x11 || pkt.getCmdId() == 0x15)) {
-        ESP_LOGI(TAG, "Parsing data packet with cmdId=0x%02x", pkt.getCmdId());
+        ESP_LOGD(TAG, "Parsing data packet with cmdId=0x%02x", pkt.getCmdId());
         pd335_sys_DisplayPropertyUpload proto_msg = pd335_sys_DisplayPropertyUpload_init_zero;
         pb_istream_t stream = pb_istream_from_buffer(pkt.getPayload().data(), pkt.getPayload().size());
         if (pb_decode(&stream, pd335_sys_DisplayPropertyUpload_fields, &proto_msg)) {
-            ESP_LOGI(TAG, "Successfully decoded protobuf message");
+            ESP_LOGD(TAG, "Successfully decoded protobuf message");
 
             // Reliable Battery Percentage
             if (proto_msg.has_cms_batt_soc) {
                 data.batteryLevel = (int)proto_msg.cms_batt_soc;
-                ESP_LOGI(TAG, "Battery level: %d%%", data.batteryLevel);
+                ESP_LOGD(TAG, "Battery level: %d%%", data.batteryLevel);
             } else {
-                ESP_LOGW(TAG, "Battery level field missing in packet");
+                ESP_LOGD(TAG, "Battery level field missing in packet");
             }
 
             // Power readings
